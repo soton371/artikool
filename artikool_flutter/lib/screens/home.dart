352eba63c760134797c6d37501af67ca5beea7a7
macbookpro <soton371@gmail.com>
2023-01-22 15:10:16 +0600

    list view

--- artikool_flutter/lib/screens/home.dart
+++ artikool_flutter/lib/screens/home.dart
@@ -1,5 +1,6 @@
-
+import 'package:artikool_client/artikool_client.dart';
 import 'package:flutter/material.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,17 +10,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var client = Client('http://localhost:8080/')
+    ..connectivityMonitor = FlutterConnectivityMonitor();
+
+  //fetch artical
+  List<Articel> articels = [];
+  Future<void> fetchArticel() async {
+    try {
+      var result = await client.artikool.getArticles();
+      print('object: ${result.length}');
+      setState(() {
+        articels = result;
+      });
+    } on Exception catch (e) {
+      debugPrint(e.toString());
+      setState(() {
+        articels = [];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchArticel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Text('data $index')
-      ),
+      body:articels.isEmpty?
+      const Text('no data')
+      :
+       ListView.builder(
+          itemCount: articels.length,
+          itemBuilder: (context, index) {
+            var data = articels[index];
+            return Card(
+              child: ListTile(
+                title: Text(data.title),
+                subtitle: Text(data.content),
+              ),
+            );
+          }),
     );
   }
 }
